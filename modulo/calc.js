1149149a567f842c90fa006fe9466d8d049747f6
Gabriel Garcia <100931438+gabrielgarcia05@users.noreply.github.com>
2023-03-14 12:24:44 -0300

    Add files via upload

---
+++ modulo/calc.js
@@ -0,0 +1,9 @@
+const somaF = require('./soma')
+const subF = require('./sub')
+const multF = require('./mult')
+const divF = require('./div')
+
+console.log("A soma é: " + somaF(3,3))
+console.log("A subtração é: " + subF(3,3))
+console.log("A multiplicação é: " + multF(3,3))
+console.log("A divisão é: " + divF(3,3))
---
+++ modulo/div.js
@@ -0,0 +1,6 @@
+
+const div = function(a,b){
+    return a/b
+}
+
+module.exports = div
